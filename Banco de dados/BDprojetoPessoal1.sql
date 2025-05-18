4857fc7f3296d2c00a58c30d51edce29684f6315
Breno-0 <breno.costa@sptech.school>
2025-05-18 00:31:31 -0300

    BD atualizado

--- Banco de dados/BDprojetoPessoal1.sql
+++ Banco de dados/BDprojetoPessoal1.sql
@@ -5,6 +5,10 @@ create table usuario (
 id int primary key auto_increment,
 nome varchar(100),
 email varchar(100),
-senha varchar(20)
+senha varchar(20),
+soad boolean,
+slipknot boolean,
+metallica boolean,
+blackSabbath boolean
 );
 
